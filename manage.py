1030a78ceed550aa3ad0e3cdeabe26e09b955d5a
BabySharon <sharonoorakathu99@gmail.com>
2018-11-14 19:30:46 -0800

    Neccessary Changes

--- manage.py
+++ manage.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request,session
+from flask import Flask, render_template, request
 
 import sqlite3
 app = Flask(__name__)
-app.secret_key = 'any random string'
 
-''' welcome link'''
-#@app.route('/home',defaults={'bookname': ""})
 @app.route('/home')
 @app.route('/home/<no>')
-#no of book that is book id or primary key
-#@app.route('/home/<bookname>')
-def home( no = None):
-	session.pop('em',None)
+'''First link directs to home page that is the page shown first. The second one is to render the same home page with the details of the book selected by the user.
+That is the details of the book are shown in the same home page with the same styling formats. This is done to avoid creating more html pages for the sake of convinence.
+'''
+#no of book that is book id or primary key that uniquely identifies a book
+
+def home( no = None):#no is a default argument that is its default value is None
 	conn = sqlite3.connect('book.db')
 	cur = conn.cursor()
 	if no == None:
@@ -19,25 +18,21 @@ def home( no = None):
 		
 	else:
 		#To dispaly full details of book
-		cur.execute("SELECT  * from book where no =?",(no,))
+		cur.execute("SELECT  * from book where no =?",(no,))#searches the database for the book with id no
 		ReqBook = cur.fetchall()
 		return render_template("home.html",ReqBook = ReqBook)
 
-@app.route("/login")
+@app.route("/login")#This route decorator renders the html page for signup or login
 def signlog():
 	return render_template("signlog.html")
 
 @app.route("/addbook", methods =['GET','POST'])
-
+'''This decorator adds user details to the database when its for signup else validates the user details in case of login. In both the cases profile page is rendered'''
 def addbook( ):
-	
 	conn = sqlite3.connect('book.db')
 	cur = conn.cursor()
-	
-	
 
-	
-	if 'signup' in request.form.keys():
+	if 'signup' in request.form.keys():#To check if the user is registering an account
 		name = request.form['name']  
 		email = request.form['email']
 		password = request.form['pswd']
@@ -50,7 +45,7 @@ def addbook( ):
 		return render_template("profile.html", msg = msg, name = name)
 				
 		
-	if 'login' in request.form.keys():
+	if 'login' in request.form.keys():#To check if the user is logging in
 		email = request.form['email']
 		password = request.form['pswd']
 		
@@ -59,15 +54,17 @@ def addbook( ):
 		login_query = cur.fetchall()
 		
 		if  not login_query:
-			msg = "Invalid User"
+			msg = "Invalid User!!!!!"
 			conn.commit()
-			return render_template("signlog.html",msg = msg)
+			return render_template("signlog.html", msg = msg)
 		else:
 			msg = "login succesful"
 			return render_template("profile.html",msg = msg, login_query = login_query)
 		
 	conn.close()				
 @app.route("/book", methods =['POST','GET'])
+'''The function below have definitions to enter a book of the user which is done from the profile page
+'''
 def book():
 	if request.method == "GET":
 		try:
@@ -109,10 +106,11 @@ def search():
 			msg = listlength + " books found" 
 			return render_template('home.html', msg = msg, res = res)
 
-@app.route('/logout')
+'''@app.route('/logout')
 def logout():
 	session.pop('em',none)
 	return render_template('home.html')
+	'''
 	
 if __name__ == "__main__":
   	app.run(host = '192.168.43.7', port = 8080, debug = True)
